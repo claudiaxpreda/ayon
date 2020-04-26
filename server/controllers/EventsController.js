95be761ea847fd7e1c8d402d5f83f220a976d6dc
Claudia PREDA (87755) <{claudia.preda2307@stud.acs.upb.ro}>
2020-04-26 03:12:11 +0300

    Do minor updates

--- server/controllers/EventsController.js
+++ server/controllers/EventsController.js
@@ -2,7 +2,111 @@ const express = require('express');
 const router = express.Router();
 
 const {
+    Users,
     Events
 } = require('../data')
+
+
+router.get('/', async(req, res)=> {
+    if (req.session.loggedin) {
+        const username = req.session.username;
+        const user = await Users.findOne({username})
+
+        if (user) {
+            const events = await Events.find({user: user.id});
+            res.status(200).send(events);
+        } else {
+            res.status(401).send('Please try again');
+        }
+    } else {
+        res.status(401).send('You are not logged in')
+    }
+});
+
+router.get('/:id', async(req, res)=> {
+    const {id} = req.params;
+    const username = req.session.username;
+
+    if (req.session.loggedin) {
+        try {
+            const event = await Events.findById(id);
+            const user = await Users.findOne({username})
+
+            if (event.user === user.id) {
+                res.status(200).send(event);
+            } else {
+                res.status(500).send('Event not found');
+            }
+        } catch (err) {
+            throw(new Error(err.message));
+        };
+    } else {
+        res.status(401).send('You are not logged in');
+    }
+});
+
+router.post('/', async(req, res)=> {
+    console.log(req.session.loggedin)
+    if (req.session.loggedin) {
+        const username = req.session.username;
+        const params = req.body;
+        try {
+            const user = await Users.findOne({username});
+            params.user = user.id;
+            const event = new Events(params);
+            await event.save();
+            res.status(200).send("Event added");
+        } catch (err) {
+            throw(new Error(err.message));
+        }
+    } else {
+        res.status(401).send('You are not logged in');
+    }
+});
+
+router.put('/:id', async(req, res)=> {
+    const params = req.body;
+    const {id} = req.params;
+    const username = req.session.username;
+    if (req.session.loggedin) {
+        try {
+            const user = Users.findOne({username});
+            const event = Events.findById(id);
+
+            if (event.user === user.id) {
+                await Events.findByIdAndUpdate(id, params);
+                res.status(200).send("Event updated");
+            } else {
+                res.status(500).send('Event not found');
+            }
+        } catch (err) {
+            throw(new Error(err.message));
+        };
+    } else {
+        res.status(401).send('You are not logged in');
+    }
+});
+
+router.delete('/:id', async(req, res)=> {
+    const {id} = req.params;
+    const username = req.session.username;
+    if (req.session.loggedin) {
+        try {
+            const user = Users.findOne({username});
+            const event = Events.findById(id);
+
+            if (event.user === user.id) {
+                await Events.findByIdAndDelete(id);
+                res.status(200).send("Event deleted");
+            } else {
+                res.status(500).send('Event not found');
+            }
+        } catch (err) {
+            throw(new Error(err.message));
+        };
+    } else {
+        res.status(401).send('You are not logged in');
+    }
+});
 
 module.exports = router;
--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -7,7 +7,6 @@ const {
 
 const {
     Users,
-    Events
 } = require('../data')
 
 
@@ -80,108 +79,6 @@ router.post('/login', async(req, res) => {
         throw(new Error(err.message));
     };
 
-});
-
-router.get('/events', async(req, res)=> {
-    if (req.session.loggedin) {
-        const username = req.session.username;
-        const user = await Users.findOne({username})
-
-        if (user) {
-            const events = await Events.find({user: user.id});
-            res.status(200).send(events);
-        } else {
-            res.status(401).send('Please try again');
-        }
-    } else {
-        res.status(401).send('You are not logged in')
-    }
-});
-
-router.get('/events/:id', async(req, res)=> {
-    const {id} = req.params;
-    const username = req.session.username;
-
-    if (req.session.loggedin) {
-        try {
-            const event = await Events.findById(id);
-            const user = await Users.findOne({username})
-
-            if (event.user === user.id) {
-                res.status(200).send(event);
-            } else {
-                res.status(500).send('Event not found');
-            }
-        } catch (err) {
-            throw(new Error(err.message));
-        };
-    } else {
-        res.status(401).send('You are not logged in');
-    }
-});
-
-router.post('/events', async(req, res)=> {
-
-    if (req.session.loggedin) {
-        const username = req.session.username;
-        const params = req.body;
-        try {
-            const user = await Users.findOne({username});
-            params.user = user.id;
-            const event = new Events(params);
-            await event.save();
-            res.status(200).send("Event added");
-        } catch (err) {
-            throw(new Error(err.message));
-        }
-    } else {
-        res.status(401).send('You are not logged in');
-    }
-});
-
-router.put('/events/:id', async(req, res)=> {
-    const params = req.body;
-    const {id} = req.params;
-    const username = req.session.username;
-    if (req.session.loggedin) {
-        try {
-            const user = Users.findOne({username});
-            const event = Events.findById(id);
-
-            if (event.user === user.id) {
-                await Events.findByIdAndUpdate(id, params);
-                res.status(200).send("Event updated");
-            } else {
-                res.status(500).send('Event not found');
-            }
-        } catch (err) {
-            throw(new Error(err.message));
-        };
-    } else {
-        res.status(401).send('You are not logged in');
-    }
-});
-
-router.delete('/events/:id', async(req, res)=> {
-    const {id} = req.params;
-    const username = req.session.username;
-    if (req.session.loggedin) {
-        try {
-            const user = Users.findOne({username});
-            const event = Events.findById(id);
-
-            if (event.user === user.id) {
-                await Events.findByIdAndDelete(id);
-                res.status(200).send("Event deleted");
-            } else {
-                res.status(500).send('Event not found');
-            }
-        } catch (err) {
-            throw(new Error(err.message));
-        };
-    } else {
-        res.status(401).send('You are not logged in');
-    }
 });
 
 module.exports = router;
